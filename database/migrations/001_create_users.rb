0bebcac90d60b318540dc5ef3c9a251eb8c04b77
Georgi Urumov <gurumov92@gmail.com>
2013-10-26 02:48:11 +0300

    Update 001_create_users.rb

--- database/migrations/001_create_users.rb
+++ database/migrations/001_create_users.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     create_table(:users) do
-      primary_key :id
+      primary_key :id, index: true
       Integer :faculty_number, uniq: true, index: true
       String :name, null: false
       String :email, null: false, unique: true
