4998a27f47cb8b5ca2ef2a1386abc457b5202d6c
Georgi <georgi@georgi-ubuntu.(none)>
2013-10-26 00:07:55 +0300

    initial print migration

--- migrations/001_create_print.rb
+++ migrations/001_create_print.rb
@@ -2,11 +2,15 @@ Sequel.migration do
   up do
     #Create Prints table
     create_table(:prints) do
-      Integer :print_id, :primary_key=>true, :auto_increment=>true
-      String :name, :null=>false
+      Integer :id, :primary_key=>true, :auto_increment=>true, :index=>true
+      Integer :pages, :null=>false
+      Float :price, :null=>false
+      String :name, :null=>false, :index=>true
       String :language, :null=>false
-      String :ISBN, :null=>false, :unique=>true
+      String :ISBN, :null=>false, :unique=>true, :index=>true
       String :Description, :text => true, :null=>false
+      constraint(:name_min_length){char_length(name) >= 2}
+      constraint(:ISBN_min_length){char_length(ISBN) >= 9}
     end
   end
 
