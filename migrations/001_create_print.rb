0637905b79a921aae47822b4596a55a4e93cf24c
Georgi Urumov <gurumov92@gmail.com>
2013-10-26 00:42:20 +0300

    Update 001_create_print.rb

--- migrations/001_create_print.rb
+++ migrations/001_create_print.rb
@@ -7,10 +7,10 @@ Sequel.migration do
       Float :price, :null=>false
       String :name, :null=>false, :index=>true
       String :language, :null=>false
-      String :ISBN, :null=>false, :unique=>true, :index=>true
+      String :isbn, :null=>false, :unique=>true, :index=>true
       String :Description, :text => true, :null=>false
       constraint(:name_min_length){char_length(name) >= 2}
-      constraint(:ISBN_min_length){char_length(ISBN) >= 9}
+      constraint(:isbn_min_length){char_length(isbn) >= 9}
     end
   end
 
