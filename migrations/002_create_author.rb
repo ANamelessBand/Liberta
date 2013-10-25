05f9dd68ac10432e20762d7a84bef28888bdd8ea
fil <fil@fil.(none)>
2013-10-26 00:33:46 +0300

    Add migrations for creating tables for print related objects

---
+++ migrations/002_create_author.rb
@@ -0,0 +1,12 @@
+Sequel.migrations do
+  up do
+    create_table(:authors) do
+      Integer :id, :primary_key=>true
+      String :name, :null=>false
+    end
+  end
+
+  down do
+    drop_table(:authors)
+  end
+end
---
+++ migrations/003_create_publisher.rb
@@ -0,0 +1,12 @@
+Sequel.migrations do
+  up do
+    create_table(:publishers) do
+      Integer :id, :primary_key=>true
+      String :name, :null=>false
+    end
+  end
+
+  down do
+    drop_table(:publishers)
+  end
+end
