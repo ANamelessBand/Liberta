70c94c9e6abcd6b112ad9af98a0dd9f97163fee4
Кристиан Ташков <kristian.tashkov@gmail.com>
2013-10-26 13:10:20 +0300

    added helper methods to application controller

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -2,4 +2,13 @@ module ApplicationHelpers
   def set_active_navigation_link(active_id)
     self.navigation_links.each { |link| link.active = link.id == active_id }
   end
+
+  def glyphicon_snap(name)
+    "<span class='glyphicon glyphicon-#{name}'></span>"
+  end
+
+  def stars_span(rating)
+    rounded_rating = (rating * 2).round / 2.0
+    "<span class='stars s-#{rounded_rating}' data-default='#{rounded_rating}'> #{rounded_rating} stars </span>"
+  end
 end
