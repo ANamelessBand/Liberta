0be6372f360580f431c4585674984bfac3b47476
Georgi Gardev <gogogardev@gmail.com>
2013-10-26 21:58:36 +0300

    Fix Print-Format relationship

--- models/sequel/print.rb
+++ models/sequel/print.rb
@@ -1,7 +1,7 @@
 class Print < Sequel::Model
   many_to_many :authors
   many_to_many :tags
-  many_to_many :formats
+  many_to_one :format
   many_to_one :publisher
   one_to_many :copies
   one_to_many :recommendations
