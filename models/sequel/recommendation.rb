9cd03b02a6dd3ae3f1640c32d3de29c181121d1b
dakatapetrov <daka7a@gmail.com>
2013-10-26 15:26:59 +0300

    Refactor recommendation model.

--- models/sequel/recommendation.rb
+++ models/sequel/recommendation.rb
@@ -1,11 +1,12 @@
 class Recommendation < Sequel::Model
   plugin :validation_helpers
-		many_to_one :users
-    many_to_one :prints
-    def validate
-      super
-      validates_presence [:user_id, :print_id, :rating]
-      validates_length_range 1..5, :rating
-      validates_integer :rating
-    end
+
+  many_to_one :users
+  many_to_one :prints
+
+  def validate
+    super
+    validates_presence [:user_id, :print_id, :rating]
+    validates_includes (1..5).to_a, :rating
+  end
 end
