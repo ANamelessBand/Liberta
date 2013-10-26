7b6fe600893e32dd18149b09f18328a7c1a7e64b
dakatapetrov <daka7a@gmail.com>
2013-10-26 20:55:15 +0300

    Add requires to the users and notifications dummy population.

--- database/dummy_data_population.rb
+++ database/dummy_data_population.rb
@@ -8,3 +8,5 @@ Dir.glob('./../models/sequel/*.rb').each { |file| require file }
 require './dummy_data_packs/dummy_authors.rb'
 require './dummy_data_packs/dummy_tags.rb'
 require "./dummy_data_packs/dummy_formats.rb"
+require "./dummy_data_packs/dummy_users.rb"
+require "./dummy_data_packs/dummy_notifications.rb"
