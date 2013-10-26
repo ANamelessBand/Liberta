237920c772a91bdd9a33dc54677a2250221b4a6a
filareta <filareta_yordanova@abv.bg>
2013-10-26 17:22:21 +0300

    fixed typo in author.rb

--- models/sequel/author.rb
+++ models/sequel/author.rb
@@ -2,8 +2,9 @@
 class Author < Sequel::Model
   many_to_many :prints
 
-  plugin :validarion_helpers
+  plugin :validation_helpers
   def validate
     super
     validates_presence :name
+  end
 end
