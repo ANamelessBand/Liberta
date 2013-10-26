99fcc2ee35a9321a0974bd33f4f8049b47983059
dakatapetrov <daka7a@gmail.com>
2013-10-26 15:25:54 +0300

    Refactor notification model.

--- models/sequel/notification.rb
+++ models/sequel/notification.rb
@@ -1,3 +1,10 @@
 class Notification < Sequel::Model
+  plugin :validation_helpers
+
   many_to_one :user
+
+  def validate
+    super
+    validates_presence [:user_id, :message, :is_read]
+  end
 end
