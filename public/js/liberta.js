d05653cd75e2ed965fccecf19832b9d3a8003b70
Georgi Gardev <gogogardev@gmail.com>
2013-10-27 12:27:02 +0200

    AJAX Add/Remove to wishlist

--- public/js/liberta.js
+++ public/js/liberta.js
@@ -24,5 +24,35 @@
       });
     });
 
+    function changeWish(operation, element) {
+      $this = $(element);
+      $id = $this.data("for")
+
+      url = '/prints/' + $id
+      if (operation == "add") {
+        url += '/add-wishlist';
+        newButton = "<a class='remove-wish btn btn-danger' data-for='"+$id+"'><span class='glyphicon glyphicon-minus'></span> премахни от желани</a>";
+      }
+      else if (operation == "remove") {
+        url += '/remove-wishlist';
+        newButton = "<a class='add-wish btn btn-success' data-for='"+$id+"'><span class='glyphicon glyphicon-plus'></span> добави в желани</a>";
+      }
+
+      $.ajax({
+        url: url,
+        success: function() {
+          $this.after(newButton);
+          $this.remove();
+        }
+      })
+    }
+
+    $(document).on("click", ".add-wish", function() {
+      changeWish("add", this);
+    })
+
+    $(document).on("click", ".remove-wish", function() {
+      changeWish("remove", this);
+    })
   })
 })(jQuery, window);
