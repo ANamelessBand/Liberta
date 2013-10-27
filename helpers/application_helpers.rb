1a2973f4fe100192b691425c824e47092a148665
dakatapetrov <daka7a@gmail.com>
2013-10-27 03:07:34 +0200

    Add basic notification logic.

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -21,6 +21,14 @@ module ApplicationHelpers
   end
 
   def logged_user
-    logged? ? session[:user] : nil
+    session[:user] if logged?
+  end
+
+  def unread_notifications
+    Notification.where(user_id: logged_user.id, is_read: false).to_a if logged?
+  end
+
+  def unread_notifications?
+    unread_notifications && unread_notifications.count.nonzero?
   end
 end
