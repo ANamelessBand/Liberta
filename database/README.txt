682958aec2f9310eca1df3fe2c61be09515be830
Georgi Gardev <gogogardev@gmail.com>
2013-11-17 17:18:06 +0200

    Create rakefile with rake db tasks. See #21

--- database/README.txt
+++
@@ -1,2 +0,0 @@
-from this directory execute "sequel -m migrations/ sqlite://liberta.db"
-from this directrory execute "ruby dummy_data_population.rb"
--- database/dummy_data_presentation.rb
+++ database/dummy_data_presentation.rb
@@ -1,19 +1,3 @@
-require 'sequel'
 require 'date'
-
-
-puts "Deleting database..."
-system 'rm ./database/liberta.db'
-puts "done"
-
-puts "Creating database"
-system 'sequel -m ./database/migrations sqlite://./database/liberta.db'
-puts "done"
-
-# Sequel.sqlite('./database/liberta.db')
-
-Sequel.postgres('liberta', :host=>'localhost', :user=>'liberta', :password=>'liberta')
-
 Dir.glob('./models/sequel/*.rb').each { |file| require file }
-
 require './database/presentation_dummy_data/dummy_data'
