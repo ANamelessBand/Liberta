ff2acb6e649851ece1c073884ac2027b37579de3
dakatapetrov <daka7a@gmail.com>
2013-11-17 17:09:01 +0200

    Refactor news model. See #7.

--- models/sequel/news.rb
+++ models/sequel/news.rb
@@ -5,4 +5,10 @@ class News < Sequel::Model
     super
     validates_presence [:title, :content, :date_of_publication]
   end
+
+  def self.newest
+    all.sort do |news_a, news_b|
+      news_b.date_of_publication <=> news_a.date_of_publication
+    end
+  end
 end
