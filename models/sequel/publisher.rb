6f12391e4c4ad31bf50aaeadd05c431c9c39705c
dakatapetrov <daka7a@gmail.com>
2013-11-17 17:04:24 +0200

    Refactor publisher model. See #7.

--- models/sequel/publisher.rb
+++ models/sequel/publisher.rb
@@ -4,6 +4,19 @@ class Publisher < Sequel::Model
   one_to_many :prints
 
   def validate
+    super
     validates_presence :name
+  end
+
+  def top_prints
+     prints.sort do |print_a, print_b|
+       print_b.rating <=> print_a.rating
+     end
+  end
+
+  def top_prints_for_last_month
+     prints.sort do |print_a, print_b|
+       print_b.rating_last_month <=> print_a.rating_last_month
+     end
   end
 end
