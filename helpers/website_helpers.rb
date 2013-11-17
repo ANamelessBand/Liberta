8d28c7b0b47e756db11bdebc2c85f66273819267
dakatapetrov <daka7a@gmail.com>
2013-11-17 17:14:11 +0200

    Refactor website helper. See #7.

--- helpers/website_helpers.rb
+++ helpers/website_helpers.rb
@@ -1,2 +1,45 @@
 module WebsiteHelpers
+  def login(username, password)
+    user = User.find(username: username)
+
+    if user
+      # TODO Implement authorization logic here
+      session[:user] = user.id
+    end
+  end
+
+  def logout
+    session[:user] = nil
+  end
+
+  def add_news(title, content)
+    date = Date.today
+    news = News.new title: title, content: content, date_of_publication: date
+
+    if news.valid?
+      news.save
+    else
+      # TODO add validation logic here
+    end
+  end
+
+  def read_notification(notification_id)
+    notification = Notification.find id: notification_id
+    notification.read!
+  end
+
+  def get_author(author_id)
+    Author.find id: author_id
+  end
+
+  def get_publisher(publisher_id)
+    Publisher.find id: publisher_id
+  end
+
+  def show_print_table(prints, ratings_last_month = false)
+    @prints_collection = prints
+    @ratings_last_month = ratings_last_month
+
+    erb :'prints_table.html'
+  end
 end
