562a35c6cf92b80b64f7f55a9bdc298811396161
dakatapetrov <daka7a@gmail.com>
2013-11-24 00:57:17 +0200

    Initial refactoring of the loan model. See #7.

--- models/sequel/loan.rb
+++ models/sequel/loan.rb
@@ -1,6 +1,11 @@
 class Loan < Sequel::Model
+  plugin :validation_helpers
+
   many_to_one :copy
   many_to_one :user
+
+  def validate
+    super
+    validates_presence [:date_loaned, :date_supposed_return]
+  end
 end
-
-
