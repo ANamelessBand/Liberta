e832f48444024f0ac1f9c952a82b1c093b4031d7
dakatapetrov <daka7a@gmail.com>
2013-11-24 00:57:02 +0200

    Initial refactoring of the copy model. See #7.

--- models/sequel/copy.rb
+++ models/sequel/copy.rb
@@ -1,4 +1,22 @@
 class Copy < Sequel::Model
   many_to_one :print
   one_to_many :loans
+
+  def take
+    is_taken = true
+  end
+
+  def take!
+    take
+    save
+  end
+
+  def return
+    is_taken = false
+  end
+
+  def return!
+    self.return
+    save
+  end
 end
