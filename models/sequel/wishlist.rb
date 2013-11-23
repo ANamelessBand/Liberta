5f3eac69ebf2cdee162817a92346273d181a0e4c
dakatapetrov <daka7a@gmail.com>
2013-11-24 01:02:41 +0200

    Initial refactoring of the wishlist model. See #7.

--- models/sequel/wishlist.rb
+++ models/sequel/wishlist.rb
@@ -1,4 +1,19 @@
 class Wishlist < Sequel::Model
   many_to_one :user
   many_to_one :print
+
+  def satisfy
+    is_satisfied = true
+  end
+
+  def satisfy!
+    satisfy
+    save
+  end
+
+  def self.satisfy_wishes(user, print)
+    Wishlist.where(user: user, print: print).each do |user_wish|
+      user_wish.satisfy!
+    end
+  end
 end
