74f63efda8240bda525f6e5c3be640ee05247def
dakatapetrov <daka7a@gmail.com>
2013-12-01 23:58:20 +0200

    Refactor application controller using the suggestions by rubocop. See #7.

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -5,17 +5,21 @@ class ApplicationController < Sinatra::Base
   helpers ApplicationHelpers
 
   before do
-    @navigation_links = [NavigationLink.new(NavigationLink.news_id,
-                                            "/news",
-                                            "Новини"),
-                        NavigationLink.new(NavigationLink.prints_id,
-                                           "/prints",
-                                           "Библиотека"),
-                        NavigationLink.new(NavigationLink.most_liked_id,
-                                           "/prints/most-liked",
-                                           "Най-харесвани"),
-                        NavigationLink.new(NavigationLink.users_id, "/users", "Потребители"),]
+    @navigation_links = [
+                          NavigationLink.new(NavigationLink.news_id,
+                                             '/news',
+                                             'Новини'),
+                          NavigationLink.new(NavigationLink.prints_id,
+                                             '/prints',
+                                             'Библиотека'),
+                          NavigationLink.new(NavigationLink.most_liked_id,
+                                             '/prints/most-liked',
+                                             'Най-харесвани'),
+                          NavigationLink.new(NavigationLink.users_id,
+                                             '/users',
+                                             'Потребители'),
+                        ]
 
-    @breadcrumbs = [NavigationLink.new(0, "/news", "Начало")]
+    @breadcrumbs = [NavigationLink.new(0, '/news', 'Начало')]
   end
 end
