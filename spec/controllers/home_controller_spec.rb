a9ed1d4c02594d5e2c852bf08d55cc36899cee9c
Georgi Gardev <georgi@gardev.com>
2019-03-17 00:33:53 +0200

    Add tests for HomeController

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,4 +3,26 @@
 require "rails_helper"
 
 RSpec.describe HomeController, type: :controller do
+  subject { get :index }
+
+  let (:news) { FactoryBot.create_list(:news, 10) }
+  let (:prints) { FactoryBot.create_list(:print, 10) }
+
+  describe "GET index" do
+    it "assigns last_news" do
+      allow(News).to receive_message_chain(:order, :last).and_return(news)
+
+      subject
+
+      expect(assigns(:last_news)).to eq news
+    end
+
+    it "assigns last_prints" do
+      allow(Print).to receive_message_chain(:order, :last).and_return(prints)
+
+      subject
+
+      expect(assigns(:last_prints)).to eq prints
+    end
+  end
 end
