636e53bec528c6d7b253e1f40f90960a25ee36b9
Georgi Gardev <georgi@gardev.com>
2019-03-17 00:12:25 +0200

    Add tests for ErrorsController

---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ErrorsController, type: :controller do
+  describe "GET not_found" do
+    setup { get :not_found }
+    it { should render_template('errors/error') }
+  end
+
+  describe "GET unacceptable" do
+    setup { get :unacceptable }
+    it { should render_template('errors/error') }
+  end
+
+  describe "GET internal_error" do
+    setup { get :internal_error }
+    it { should render_template('errors/error') }
+  end
+end
