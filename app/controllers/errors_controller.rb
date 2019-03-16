899be4b58d2d0bfcae9887a9fc3901050992d184
Georgi Gardev <georgi@gardev.com>
2019-03-16 20:06:24 +0200

    Fix lint issues

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class ErrorsController < ApplicationController
   def not_found
-    render 'error', status: 404
+    render "error", status: 404
   end
 
   def unacceptable
-    render 'error', status: 422
+    render "error", status: 422
   end
 
   def internal_error
-    render 'error', status: 500
+    render "error", status: 500
   end
 end
